7a0c711c83bf7d22865b44abaa8de15757c19552
ForzarAppS <fas.offical@yandex.ru>
2020-08-12 20:02:22 +0300

    12_08

--- resources/scripts/onload-page.js
+++ resources/scripts/onload-page.js
@@ -34,27 +34,46 @@ function PageOnload() {
             
             if (event.target.scrollTop <= (document.getElementById("our-projects").getBoundingClientRect().top + event.target.scrollTop - 300)) {
                 document.getElementsByClassName("nav-sections")[0].style.borderBottom = "2px solid";
+                
                 document.getElementsByClassName("nav-sections")[1].style.borderBottom = "none";
+                
                 document.getElementsByClassName("nav-sections")[2].style.borderBottom = "none";
+                
                 document.getElementsByClassName("nav-sections")[3].style.borderBottom = "none";
             }
             else if (event.target.scrollTop <= (document.getElementById("about-us").getBoundingClientRect().top + event.target.scrollTop - 300)) {
                 document.getElementsByClassName("nav-sections")[0].style.borderBottom = "none";
+                
                 document.getElementsByClassName("nav-sections")[1].style.borderBottom = "2px solid";
+                
                 document.getElementsByClassName("nav-sections")[2].style.borderBottom = "none";
+                
                 document.getElementsByClassName("nav-sections")[3].style.borderBottom = "none";
             }
             else if (event.target.scrollTop <= (document.getElementById("contacts").getBoundingClientRect().top + event.target.scrollTop - 400)) {
                 document.getElementsByClassName("nav-sections")[0].style.borderBottom = "none";
+                
                 document.getElementsByClassName("nav-sections")[1].style.borderBottom = "none";
+                
                 document.getElementsByClassName("nav-sections")[2].style.borderBottom = "2px solid";
+                
                 document.getElementsByClassName("nav-sections")[3].style.borderBottom = "none";
             }
             else {
                 document.getElementsByClassName("nav-sections")[0].style.borderBottom = "none";
+                
                 document.getElementsByClassName("nav-sections")[1].style.borderBottom = "none";
+                
                 document.getElementsByClassName("nav-sections")[2].style.borderBottom = "none";
+                
                 document.getElementsByClassName("nav-sections")[3].style.borderBottom = "2px solid";
+            }
+
+            if (event.target.scrollTop >= (document.getElementsByTagName("footer")[0].getBoundingClientRect().top + event.target.scrollTop - 800)) {
+                document.getElementsByClassName("up")[0].style.bottom = (800 - document.getElementsByTagName("footer")[0].getBoundingClientRect().top) + "px";
+            }
+            else {
+                document.getElementsByClassName("up")[0].style.bottom = "5px";
             }
         });
     }
